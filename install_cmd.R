eecd04ffe9455655b6f0f5fbae9d9d4219f320e9
seqyuan <ahworld07@gmail.com>
2024-02-06 15:29:45 +0800

    c

--- install_cmd.R
+++ install_cmd.R
@@ -1,6 +1,7 @@
 
 install.packages(c('ggplot2', 'dplyr', 'BPCells', 'presto', 'glmGamPoi', 'rjson', 'viridis', 'reticulate', 'cowplot'))
-remotes::install_version('Seurat', '4.1.4', repos = c("https://satijalab.r-universe.dev", getOption('repos')))
+remotes::install_version("SeuratObject", "4.1.4", repos = c("https://satijalab.r-universe.dev", getOption("repos")))
+remotes::install_version(package = 'Seurat', version = package_version('4.4.0'))
 devtools::install_version("Matrix",version = "1.6.1.1", INSTALL_opts = '--no-lock', force=TRUE)
 remotes::install_github('satijalab/seurat-data', quiet = TRUE)
 remotes::install_github('satijalab/azimuth', quiet = TRUE)
