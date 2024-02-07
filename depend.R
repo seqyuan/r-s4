fcde8f44b50bd893ba418c8ee357ce810fbc9d2d
seqyuan <ahworld07@gmail.com>
2024-02-07 17:38:50 +0800

    v0

---
+++ depend.R
@@ -0,0 +1,5 @@
+conda install -y -c bioconda bioconductor-tfbstools
+conda install -y -c bioconda bioconductor-dirichletmultinomial
+conda install -y -c bioconda r-htmltools=0.5.7
+conda install -y -c conda-forge r-ggiraph
+conda install -y -c r r-ggiraphextra
