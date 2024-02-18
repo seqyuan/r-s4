b1140ca55d246cea0765892ad54912b9b1912a33
seqyuan <ahworld07@gmail.com>
2024-02-18 08:55:21 +0800

    GITHUB_PAT

--- depend.sh
+++ depend.sh
@@ -3,3 +3,4 @@ conda install -y -c bioconda bioconductor-dirichletmultinomial
 conda install -y -c bioconda r-htmltools=0.5.7
 conda install -y -c conda-forge r-ggiraph
 conda install -y -c r r-ggiraphextra
+echo "GITHUB_PAT=8ef1xxxxxxx50930dxxxx5d9e4982" >>~/.Renviron
